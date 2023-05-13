283adf878826fae5832cb6946fb536680be9f252
Bar Nachlieli <bar2133@gmail.com>
2023-05-13 11:41:26 +0300

    add setup.py and package requirments

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plate-license-recognizer',
+    version='1.0.0',
+    packages=find_packages(include=['classes']),
+    url='',
+    license='MIT',
+    author='barnachlieli',
+    author_email='',
+    description=''
+)
